2fda27d7342883d5b38474ff93440d64336c6430
siutin <osiutino@gmail.com>
2019-12-27 01:53:49 +0800

    webpack config - way to import legacy js files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const MergeIntoSingleFilePlugin = require('webpack-merge-and-include-globally');
+
 const path = require('path');
 
 module.exports = {
@@ -9,6 +11,67 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin(['index.html']),
+    new MergeIntoSingleFilePlugin({
+      files: {
+        "core.js": [
+          "src/const.js",
+          "src/config.js",
+          "src/io.js",
+          "src/main.js",
+          "src/lib.js",
+          "src/fpu.js",
+          "src/ide.js",
+          "src/pci.js",
+          "src/floppy.js",
+          "src/memory.js",
+          "src/dma.js",
+          "src/pit.js",
+          "src/vga.js",
+          "src/ps2.js",
+          "src/pic.js",
+          "src/rtc.js",
+          "src/uart.js",
+          "src/hpet.js",
+          "src/acpi.js",
+          "src/apic.js",
+          "src/ioapic.js",
+          "src/state.js",
+          "src/ne2k.js",
+          "src/sb16.js",
+          "src/virtio.js",
+          "src/bus.js",
+          "src/log.js",
+          "src/cpu.js",
+          "src/translate.js",
+          "src/modrm.js",
+          "src/string.js",
+          "src/arith.js",
+          "src/misc_instr.js",
+          "src/instructions.js",
+          "src/debug.js",
+          "src/elf.js"
+        ],
+        "lib.js": [
+          "lib/9p.js",
+          "lib/filesystem.js",
+          "lib/jor1k.js",
+          "lib/marshall.js",
+          "lib/utf8.js"
+        ],
+        "browser.js": [
+          "src/browser/screen.js",
+          "src/browser/keyboard.js",
+          "src/browser/mouse.js",
+          "src/browser/speaker.js",
+          "src/browser/serial.js",
+          "src/browser/network.js",
+          "src/browser/lib.js",
+          "src/browser/starter.js",
+          "src/browser/worker_bus.js",
+          "src/browser/dummy_screen.js"
+        ]
+      }
+})
   ],
 };
