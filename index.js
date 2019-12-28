216344edc5a32f5d5c01978a22c70601b3c673a2
siutin <osiutino@gmail.com>
2019-12-29 01:13:13 +0800

    workable import way

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 import * as wasm from "v86-wasm";
 wasm.greet('Martin');
+
+import {
+    CPU_LOG_VERBOSE,
+    CPU,
+    CPU_EX_P,
+    CPU_EX_U,
+    CPU_EX_O,
+    CPU_EX_Z,
+    CPU_EX_D,
+    CPU_EX_I
+} from './src/cpu.js';
+window.CPU = CPU
+window.CPU_EX_P = CPU_EX_P
+window.CPU_EX_U = CPU_EX_U
+window.CPU_EX_O = CPU_EX_O
+window.CPU_EX_Z = CPU_EX_Z
+window.CPU_EX_D = CPU_EX_D
+window.CPU_EX_I = CPU_EX_I
+
+import './src/debug.js';
+import './src/memory.js';
+import './src/state.js';
+import './src/modrm.js';
+import * as String from'./src/string.js';
+window.cmpsb = String.cmpsb;
+window.cmpsw = String.cmpsw;
+window.cmpsd = String.cmpsd;
+window.stosb = String.stosb;
+window.stosw = String.stosw;
+window.stosd = String.stosd;
+window.lodsb = String.lodsb;
+window.lodsw = String.lodsw;
+window.lodsd = String.lodsd;
+window.scasb = String.scasb;
+window.scasw = String.scasw;
+window.scasd = String.scasd;
+window.insb = String.insb;
+window.insw = String.insw;
+window.insd = String.insd;
+window.outsb = String.outsb;
+window.outsw = String.outsw;
+window.outsd = String.outsd;
+
+import './src/arith.js';
+import './src/misc_instr.js';
+
+import './src/instructions.js';
+
